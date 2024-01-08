245c6b564920117e800e5f2aa2181fdf9d3d51ee
Macarena <a_macrogege@iestriana.com>
2024-01-08 17:32:46 +0100

    el proyecto xD

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.LogError("Test Error");
+        Debug.Log("Hello World");
+        Debug.LogWarning("Cuidao bro");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
